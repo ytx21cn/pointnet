eee7cfab95320abce0e34513ba553463d787b2b0
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-07-11 17:37:20 -0700

    revised shell scripts to add python 3.7

---
+++ install-python-3.7.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Install Python 3.7
+# please make sure that you have already added Python 3.7 to your apt repository.
+sudo apt install python3.7 python3.7-dbg python3.7-dev python3.7-venv
