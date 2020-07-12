15c3a3bb938d9fe647663afba2da76ead8499dd6
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-07-12 11:27:37 -0700

    close h5py after data are loaded

--- provider.py
+++ provider.py
@@ -91,9 +91,10 @@ def get_data_files(list_filename):
 
 
 def load_h5(h5_filename):
-    f = h5py.File(h5_filename)
+    f = h5py.File(h5_filename, 'r')
     data = f['data'][:]
     label = f['label'][:]
+    f.close()
     return data, label
 
 
@@ -106,6 +107,7 @@ def load_h5_data_label_seg(h5_filename):
     data = f['data'][:]
     label = f['label'][:]
     seg = f['pid'][:]
+    f.close()
     return data, label, seg
 
 
