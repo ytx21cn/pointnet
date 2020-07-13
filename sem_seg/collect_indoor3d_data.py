28a3b6cbc4e76f6335bdd568659e91ba016efc71
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-07-12 18:02:54 -0700

    moved around imports

--- sem_seg/collect_indoor3d_data.py
+++ sem_seg/collect_indoor3d_data.py
@@ -1,14 +1,15 @@
 import os
 import sys
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = os.path.dirname(BASE_DIR)
-sys.path.append(BASE_DIR)
 import indoor3d_util
 
 anno_paths = [line.rstrip() for line in open(os.path.join(BASE_DIR, 'meta/anno_paths.txt'))]
 anno_paths = [os.path.join(indoor3d_util.DATA_PATH, p) for p in anno_paths]
 
-output_folder = os.path.join(ROOT_DIR, 'data/stanford_indoor3d') 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(BASE_DIR)
+
+output_folder = os.path.join(ROOT_DIR, 'data/stanford_indoor3d')
 if not os.path.exists(output_folder):
     os.mkdir(output_folder)
 
